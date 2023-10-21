450afb8665a272b434af3c99d617b0dabc38fd70
PIETRO GIOVANNI PANZERI <PIETRO.PANZERI@issgreppi.it>
2023-10-21 09:58:15 +0200

    grafica pagina principale e creazione model view controller

---
+++ GameClient/Controller/MainPageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.VisualBasic;
+
+namespace GameClient.Controller
+{
+    public partial class MainPageController : ObservableObject
+    {
+        [RelayCommand]
+        public async Task MessageTest()
+        {
+            await App.Current.MainPage.DisplayAlert("Funge", "1", "ok");
+        }
+    }
+}
---
+++ GameClient/View/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+using GameClient.Controller;
+
+namespace GameClient.View;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}

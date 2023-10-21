d38378d49c706a51590473c3d4bd854a5fabf643
EURedBoy <arosioalex06@gmail.com>
2023-10-21 17:27:11 +0200

    +Fixed database query

--- GameServer/manager/DatabaseController.cs
+++ GameServer/manager/DatabaseController.cs
@@ -15,43 +15,41 @@ public class DatabaseController
                                                 );
                                                 """;
     
-    private static readonly string CONNECTION_STRING = @"server=localhost;userid=root;database=TicTacToe";
+    private static readonly string CONNECTION_STRING = @"server=localhost;userid=root;database=tictactoe";
 
-    private static readonly string PLAYER_EXIST = "SELECT COUNT(*) FROM User WHERE username = @Username";
+    private static readonly string PLAYER_EXIST = "SELECT * FROM User WHERE username = @Username";
     private static readonly string CREATE_PLAYER = "INSERT INTO User (username, point) VALUES (@Username, 0)";
     private static readonly string GET_PLAYER = "SELECT * FROM User WHERE username = @Username";
     
     private GameController _gameController;
-    private MySqlConnection connection;
 
     public DatabaseController(GameController gameController)
     {
         _gameController = gameController;
-        connection = new MySqlConnection(CONNECTION_STRING);
-        connection.Open();
         Init();
     }
 
     private void Init()
     {
-        Task.Run(async () =>
+        ExecuteQuery(conn =>
         {
             try
             {
-                //using (MySqlConnection connection = await GetConnection())
-                //{
-                    MySqlCommand command = new MySqlCommand();
-                    command.Connection = connection;
-                    command.CommandText = CREATE_USER_TABLE;
+                MySqlCommand command = new MySqlCommand();
+                command.Connection = conn;
+                command.CommandText = CREATE_USER_TABLE;
 
-                    command.ExecuteNonQuery();
-                //}
+                command.ExecuteNonQuery();
+                
+                return false;
             }
             catch (Exception ex)
             {
                 MessageUtils.Send("Errore nella creazione della tabella: " + ex.Message, ConsoleColor.Red);
+                
+                return false;
             }
-        });
+        }).Wait();
     }
     
     public async Task<Player> LoadPlayer(string username)
@@ -61,86 +59,96 @@ public class DatabaseController
     }
 
 
-    private async Task<Player> GetPlayer(String username)
+    private async Task<Player> GetPlayer(string username)
     {
-        try
+        return await ExecuteQuery<Player>(conn =>
         {
-            //using (MySqlConnection connection = await GetConnection())
-            //{
+            try
+            {
                 MySqlCommand command = new MySqlCommand();
-                command.Connection = connection;
+                command.Connection = conn;
                 command.CommandText = GET_PLAYER;
                 command.Parameters.AddWithValue("@Username", username);
 
-                await using (MySqlDataReader reader = command.ExecuteReader())
+                using (MySqlDataReader reader = command.ExecuteReader())
                 {
                     if (reader.Read())
                     {
                         int id = reader.GetInt32(reader.GetOrdinal("id"));
                         string resultUsername = reader.GetString(reader.GetOrdinal("username"));
-                        int points = reader.GetInt32(reader.GetOrdinal("point"));
+                        int points = reader.IsDBNull(reader.GetOrdinal("point")) ? 0 : reader.GetInt32(reader.GetOrdinal("point"));
 
                         return new Player { Id = id, UserName = resultUsername, Points = points };
                     }
 
                     return null;
                 }
-            //}
-        }
-        catch (Exception ignore)
-        {
-            return null;
-        }
+            }
+            catch (Exception ex)
+            {
+                MessageUtils.Send("Errore durante il retrive dell'utente: " + ex.Message, ConsoleColor.Red);
+                throw;
+            }
+        });
     }
     private async Task<bool> CreateUser(string username)
     {
-        try
+        return await ExecuteQuery(conn =>
         {
-            //using (MySqlConnection connection = await GetConnection())
-            //{
+            try
+            {
                 MySqlCommand command = new MySqlCommand();
-                command.Connection = connection;
+                command.Connection = conn;
                 command.CommandText = CREATE_PLAYER;
                 command.Parameters.AddWithValue("@Username", username);
-
-                await connection.OpenAsync();
-
-                int rowsAffected = await command.ExecuteNonQueryAsync();
-
+                
+                int rowsAffected = command.ExecuteNonQuery();
                 return rowsAffected > 0;
-            //}
-        }
-        catch (Exception ex)
-        {
-            MessageUtils.Send("Errore nella creazione dell'utente: " + ex.Message, ConsoleColor.Red);
-            return false;
-        }
+            }
+            catch (Exception ex)
+            {
+                MessageUtils.Send("Errore nella creazione dell'utente: " + ex.Message, ConsoleColor.Red);
+                return false;
+            }
+        });
     }
-
-    private async Task<bool> PlayerExists(String username)
+    private async Task<bool> PlayerExists(string username)
     {
-        try
+        return await ExecuteQuery(conn =>
         {
-            //using (MySqlConnection connection = await GetConnection())
-            //{
+            try
+            {
                 MySqlCommand command = new MySqlCommand();
-                command.Connection = connection;
+                command.Connection = conn;
                 command.CommandText = PLAYER_EXIST;
                 command.Parameters.AddWithValue("@Username", username);
 
-                return command.ExecuteNonQuery() > 0;
-            //}
+                return command.ExecuteNonQuery() != 0;
+            }
+            catch (Exception ex)
+            {
+                MessageUtils.Send("Errore nell controllo se il player esiste: " + ex.Message, ConsoleColor.Red);
+                return false;
+            }
+        });
+    }
+
+    private async Task<R> ExecuteQuery<R>(Func<MySqlConnection, R> queryFunc)
+    {
+        try
+        {
+            await using (MySqlConnection connection = new MySqlConnection(CONNECTION_STRING))
+            {
+                await connection.OpenAsync();
+                R result = queryFunc.Invoke(connection);
+                return result;
+            }
         }
         catch (Exception ex)
         {
-            MessageUtils.Send("Errore nell controllo se il player esiste: " + ex.Message, ConsoleColor.Red);
-            return false;
+            MessageUtils.Send("Errore generale \nTipo: " + typeof(R) + "\nErrore: " + ex.Message, ConsoleColor.Red);
+            return default;
         }
-    }
-
-    private async Task<MySqlConnection> GetConnection()
-    {
-        return connection;
     }
     
 }

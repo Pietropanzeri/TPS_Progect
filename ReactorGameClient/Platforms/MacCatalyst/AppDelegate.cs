d485716ca902c05272c449feb0e2a9cd2107493e
EURedBoy <69308679+EURedBoy@users.noreply.github.com>
2023-10-24 22:50:03 +0200

    Delete ReactorGameClient directory (Perchè è qui?)

--- ReactorGameClient/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ReactorGameClient;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}

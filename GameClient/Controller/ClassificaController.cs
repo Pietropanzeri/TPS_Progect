a34c20b0205f005e1aa453c465900bf456b6e69b
PIETRO GIOVANNI PANZERI <PIETRO.PANZERI@issgreppi.it>
2023-10-30 21:51:32 +0100

    classifica fix :>

--- GameClient/Controller/ClassificaController.cs
+++ GameClient/Controller/ClassificaController.cs
@@ -1,15 +1,21 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using GameClient.model;
+using GameClient.Model;
 
 namespace GameClient.Controller;
 
 public partial class ClassificaController : ObservableObject
 {
-    public ObservableCollection<Player> TopPlayers { get; set; } = new ();
+    public ObservableCollection<TopPlayer> TopPlayers { get; set; } = new ();
 
     public ClassificaController(List<Player> topPlayers)
     {
-        topPlayers.ForEach(player => this.TopPlayers.Add(player));
+        int num = 0;
+        topPlayers.Select(player => {
+            TopPlayer topp = new TopPlayer(player);
+            topp.Position = ++num;
+            return topp;
+        }).ToList().ForEach(player => this.TopPlayers.Add(player));
     }
 }
---
+++ GameClient/Model/users/TopPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using GameClient.model;
+
+namespace GameClient.model
+{
+    public partial class TopPlayer : Player
+    {
+        [ObservableProperty]
+        private int position;
+
+        public TopPlayer(Player player)
+        {
+            Id = player.Id;
+            UserName = player.UserName;
+            Symbol = player.Symbol;
+            Points = player.Points;
+            SocketId = player.SocketId;
+        }
+    }
+}

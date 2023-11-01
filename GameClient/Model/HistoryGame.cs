3041935ed8a2da399747bd012a368324813fe897
EURedBoy <arosioalex06@gmail.com>
2023-11-01 17:36:08 +0100

    HistoryGame model

---
+++ GameClient/Model/HistoryGame.cs
@@ -0,0 +1,14 @@
+using GameClient.model;
+
+namespace GameClient.Model;
+
+public class HistoryGame
+{
+    private Player _player1;
+    private Player _player2;
+
+    private Player winner;
+
+    private DateTime startTime;
+    private DateTime endTime;
+}
